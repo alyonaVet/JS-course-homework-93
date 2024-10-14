ff32d04d280e0f68aef753c348368a986daf4d99
alyonaVet <a.v.alenka2015@gmail.com>
2024-10-14 12:12:00 +0600

    added user login, logout sessions;

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,11 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Post, Req, UseGuards } from '@nestjs/common';
 import { User, UserDocument } from '../schemas/user.schema';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { CreateUserDto } from './create-user.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
+import { TokenAuthGuard } from '../auth/token-auth.guard';
 
 @Controller('users')
 export class UsersController {
@@ -20,5 +23,20 @@ export class UsersController {
     user.generateToken();
 
     return await user.save();
+  }
+
+  @UseGuards(AuthGuard('local'))
+  @Post('sessions')
+  async login(@Req() req: Request) {
+    return req.user;
+  }
+
+  @UseGuards(TokenAuthGuard)
+  @Delete('sessions')
+  async logout(@Req() req: Request) {
+    const user = req.user as UserDocument;
+    user.generateToken();
+    await user.save();
+    return;
   }
 }
