2161be75113ce6f5a0a736967f149334d7e506f5
alyonaVet <a.v.alenka2015@gmail.com>
2024-10-14 10:45:20 +0600

    added: UsersController; User, UserSchema to MongooseModule;

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,8 @@ import { AlbumsController } from './albums/albums.controller';
 import { Album, AlbumSchema } from './schemas/album.schema';
 import { TracksController } from './tracks/tracks.controller';
 import { Track, TrackSchema } from './schemas/track.schema';
+import { UsersController } from './users/users.controller';
+import { User, UserSchema } from './schemas/user.schema';
 
 @Module({
   imports: [
@@ -16,9 +18,10 @@ import { Track, TrackSchema } from './schemas/track.schema';
       { name: Artist.name, schema: ArtistSchema },
       { name: Album.name, schema: AlbumSchema },
       {name: Track.name, schema: TrackSchema },
+      {name: User.name, schema: UserSchema },
     ]),
   ],
-  controllers: [AppController, ArtistsController, AlbumsController, TracksController],
+  controllers: [AppController, ArtistsController, AlbumsController, TracksController, UsersController],
   providers: [AppService],
 })
 export class AppModule {
