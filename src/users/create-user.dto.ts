c57abae7747222b0e4b132fe018914510b764917
alyonaVet <a.v.alenka2015@gmail.com>
2024-10-15 16:27:59 +0600

    added class-validator decorators;

--- src/users/create-user.dto.ts
+++ src/users/create-user.dto.ts
@@ -1,5 +1,16 @@
+import { IsNotEmpty, IsString, MinLength } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
   username: string;
+
+  @IsString()
+  @MinLength(5)
+  @IsNotEmpty()
   password: string;
+
+  @IsString()
+  @IsNotEmpty()
   displayName: string;
 }
